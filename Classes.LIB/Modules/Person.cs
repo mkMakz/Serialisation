84ac7b35d06b276dec4979edd51038916872c37c
mkMakz <33726337+mkMakz@users.noreply.github.com>
2018-06-04 09:38:21 +0600

    Add project files.

---
+++ Classes.LIB/Modules/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Classes.LIB.Modules
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string TellephoneNumber { get; set; }
+        public string DateOfBirth { get; set; }
+
+        [NonSerialized]
+        public List<Person> person = new List<Person>();
+
+        public Person[] persons;
+        public void FilPerson()
+        {
+            persons = person.ToArray();
+        }
+    }
+}
